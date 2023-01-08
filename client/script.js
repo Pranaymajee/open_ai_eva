35005e6e11bf8dcd99dec3d8dd0be5bf44e9e1ce
Pranay Majee <84398231+Pranaymajee@users.noreply.github.com>
2023-01-09 00:26:05 +0530

    Updated the response URL for live view

--- client/script.js
+++ client/script.js
@@ -91,7 +91,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch("https://eva-yikv.onrender.com", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
